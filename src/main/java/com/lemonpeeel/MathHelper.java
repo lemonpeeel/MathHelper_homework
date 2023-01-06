577c4275498e59c463f194b3b6363e3d001978ae
lemonpeeel <lemonpeel235@gmail.com>
2023-01-06 12:13:50 +0900

    feat: Add case 2 using PythagoreanTheorem method

--- src/main/java/com/lemonpeeel/MathHelper.java
+++ src/main/java/com/lemonpeeel/MathHelper.java
@@ -4,17 +4,17 @@ public class MathHelper {
 
     public static double[] quadraticFormula (String firstString, String secondString, String thirdString) {
 
-        double a = Double.parseDouble(firstString); //a = coefficient of x squared, 이차항계수
-        double b = Double.parseDouble(secondString);//b = coefficient of x, 일차항계수
-        double c = Double.parseDouble(thirdString);//c = constant, 상수항
+        double a = Double.parseDouble(firstString); //a = coefficient of x squared,
+        double b = Double.parseDouble(secondString);//b = coefficient of x,
+        double c = Double.parseDouble(thirdString);//c = constant,
 
-        double determinant = Math.pow(b, 2) - 4 * a * c;//판별식
+        double determinant = Math.pow(b, 2) - 4 * a * c;//
 
         if (determinant == 0) {
-            return new double[] {(-1*b) / (2*a)};//중근
+            return new double[] {(-1*b) / (2*a)};//
         }
         else {
-            double firstRoot = (-1*b + Math.sqrt(determinant)) / (2*a);//Math.sqrt: 제곱근 연산자
+            double firstRoot = (-1*b + Math.sqrt(determinant)) / (2*a);//Math.sqrt:
             double secondRoot = (-1*b - Math.sqrt(determinant)) / (2*a);
 
             return new double[]{firstRoot, secondRoot};
@@ -25,9 +25,9 @@ public class MathHelper {
 
     public static double PythagoreanTheorem (String firstString, String secondString, String thirdString) {
 
-        double a = Double.parseDouble(firstString);//a = height of Triangle, 높이
-        double b = Double.parseDouble(secondString);//b = base of Triangle, 밑변
-        double c = Double.parseDouble(thirdString);//c = hypotenuseOfTriangle, 빗변
+        double a = Double.parseDouble(firstString);//a = height of Triangle,
+        double b = Double.parseDouble(secondString);//b = base of Triangle,
+        double c = Double.parseDouble(thirdString);//c = hypotenuseOfTriangle,
 
         if (Math.abs(a) + Math.abs(b) > Math.abs(c)) {
             double resultsOfPythagoreanOperation =
