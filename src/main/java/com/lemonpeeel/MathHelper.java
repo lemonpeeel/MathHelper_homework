af31347ba0fee2507377e196a3354599f918961d
lemonpeeel <lemonpeel235@gmail.com>
2023-01-06 12:46:05 +0900

    refactor: Exchange if-else statement conditions docs: Modify docs to fit the format

--- src/main/java/com/lemonpeeel/MathHelper.java
+++ src/main/java/com/lemonpeeel/MathHelper.java
@@ -4,17 +4,22 @@ public class MathHelper {
 
     public static double[] quadraticFormula (String firstString, String secondString, String thirdString) {
 
-        double a = Double.parseDouble(firstString); //a = coefficient of x squared,
-        double b = Double.parseDouble(secondString);//b = coefficient of x,
-        double c = Double.parseDouble(thirdString);//c = constant,
+        // Double a is coefficient of x squared.
+        double a = Double.parseDouble(firstString);
 
-        double determinant = Math.pow(b, 2) - 4 * a * c;//
+        // Double b is coefficient of x.
+        double b = Double.parseDouble(secondString);
+
+        // Double c is constant.
+        double c = Double.parseDouble(thirdString);
+
+        double determinant = Math.pow(b, 2) - 4 * a * c;
 
         if (determinant == 0) {
-            return new double[] {(-1*b) / (2*a)};//
+            return new double[] {(-1*b) / (2*a)};
         }
         else {
-            double firstRoot = (-1*b + Math.sqrt(determinant)) / (2*a);//Math.sqrt:
+            double firstRoot = (-1*b + Math.sqrt(determinant)) / (2*a);
             double secondRoot = (-1*b - Math.sqrt(determinant)) / (2*a);
 
             return new double[]{firstRoot, secondRoot};
@@ -25,11 +30,23 @@ public class MathHelper {
 
     public static double PythagoreanTheorem (String firstString, String secondString, String thirdString) {
 
-        double a = Double.parseDouble(firstString);//a = height of Triangle,
-        double b = Double.parseDouble(secondString);//b = base of Triangle,
-        double c = Double.parseDouble(thirdString);//c = hypotenuseOfTriangle,
+        // Double a is height of Triangle.
+        double a = Double.parseDouble(firstString);
 
-        if (Math.abs(a) + Math.abs(b) > Math.abs(c)) {
+        // Double b is base of Triangle.
+        double b = Double.parseDouble(secondString);
+
+        // Double c is hypotenuseOfTriangle.
+        double c = Double.parseDouble(thirdString);
+
+        if (Math.abs(a) + Math.abs(b) <= Math.abs(c)) {
+
+            System.out.println("It's not a triangle.");
+            return a + b - c;
+
+        }
+        else {
+
             double resultsOfPythagoreanOperation =
                     Math.pow(c, 2) - Math.pow(a, 2) - Math.pow(b, 2);
 
@@ -49,10 +66,6 @@ public class MathHelper {
                 return resultsOfPythagoreanOperation;
             }
 
-        }
-        else {
-            System.out.println("It's not a triangle.");
-            return a + b - c;
         }
     }
 
